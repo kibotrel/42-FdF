f313e65cd3e7d0130247c907cf3cb77d919e4200
Kilian BOTREL <kibotrel@e1r5p13.42.fr>
2019-02-07 09:48:10 +0100

    Made Macro easier to understand

--- includes/env.h
+++ includes/env.h
@@ -6,15 +6,25 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/07 04:53:11 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/02/07 05:50:23 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/02/07 08:31:35 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENV_H
 # define ENV_H
 
-# define HEIGHT 600
-# define WIDTH 800
+/*
+**	Informations about mlx window
+*/
+
+# define HEIGHT 1200
+# define WIDTH 1600
 # define TITLE "Work in progress."
+
+/*
+**	Projection availables
+*/
+
+# define ISO 0
 
 #endif
