1a53be4825653b11206000e86ab2de031d96194d
Kilian BOTREL <kibotrel@e1r5p13.42.fr>
2019-02-07 06:46:17 +0100

    Setup mlx - cam - env

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 22:31:10 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/02/05 02:07:37 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/02/07 05:27:00 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <stdlib.h>
 #include "libft.h"
 #include "fdf.h"
+#include "mlx.h"
 
 static void	print_usage(void)
 {
@@ -27,7 +28,7 @@ static void	print_usage(void)
 	**	Error 6 : Missing points
 	** 	Error 7 : Empty row
 	**	Error 8 : Format
-
+	**	Error 9 : Empty File
 	*/
 
 	ft_putendl("usage: Work in Progress.");
@@ -36,18 +37,23 @@ static void	print_usage(void)
 void		print_error(char *desc, int error)
 {
 	ft_putendl(desc);
+	printf("\n=================================\n\n"); //
 	exit(error);
 }
 
 int			main(int ac, char **av)
 {
 	t_env	*env;
-
 	if (ac == 2)
 	{
+		printf("==========[ wireframe ]==========\n\n"); //
 		if (!(env = (t_env*)malloc(sizeof(t_env))))
 			print_error("Error : Can't allocate memory.", 1);
 		parse_file(av[1], env);
+		initialize(env);
+		print_map(env);
+		printf("\n=================================\n\n"); //
+		mlx_loop(env->mlx);
 	}
 	else
 		print_usage();
