7fd3ea8d7a78bd9e81930dd9c3e0c551619d8d2b
Kilian BOTREL <kibotrel@e2r3p13.42.fr>
2019-02-08 05:03:03 +0100

    Added Parallel projection and some focus around point of view

--- srcs/projection.c
+++ srcs/projection.c
@@ -6,9 +6,11 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/07 07:52:39 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/02/07 09:05:13 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/02/08 04:43:38 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 #include <math.h>
 #include "fdf.h"
@@ -21,19 +23,41 @@ static t_pos	isometric(t_pos p)
 
 	y = p.y;
 	x = p.x;
+	p.z += 50;
 	p.y = -p.z + (x + y) * sin(0.523599);
 	p.x = (x - y) * cos(0.523599);
 	return (p);
-
 }
 
-t_pos			transform(t_pos p, t_env *env)
+static t_pos	parallel(t_pos p)
+{
+	int y;
+	int x;
+
+	y = p.y;
+	x = p.x;
+	p.y = y - p.z * sin(0.785398);
+	p.x = x - p.z * cos(0.785398);
+	return (p);
+}
+
+static t_pos	pre_projection(t_pos p, t_env *env)
 {
 	p.y *= env->cam->zoom;
 	p.x *= env->cam->zoom;
-	if (env->cam->type == ISO)
+	p.z *= env->cam->zoom / env->cam->depth;
+	p.x -= (env->width * env->cam->zoom) / 2;
+	p.y -= (env->height * env->cam->zoom) / 2;
+	return (p);
+}
+t_pos			transform(t_pos p, t_env *env)
+{
+	p = pre_projection(p, env);
+	if (env->cam->type == ISOMETRIC)
 		p = isometric(p);
-	p.y += HEIGHT / 2;
-	p.x += WIDTH / 2;
+	if (env->cam->type == PARALLEL)
+		p = parallel(p);
+	p.y += HEIGHT / 2 + env->cam->offset_y;
+	p.x += WIDTH / 2 + env->cam->offset_x;
 	return (p);
 }
