948d4373fd182adc773e0d58c7d93b56b7d4850b
Kilian BOTREL <kibotrel@e2r3p13.42.fr>
2019-02-08 05:03:27 +0100

    Removed printfs for a while

--- srcs/print_map.c
+++ srcs/print_map.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/07 05:16:41 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/02/07 09:35:30 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/02/08 00:59:52 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,26 @@ void	print_map(t_env *env)
 	int	y;
 	int	x;
 
-	env->cam->zoom = ((WIDTH / env->width / 2) < (HEIGHT / env->height / 2) ? (WIDTH / env->width / 2) : (HEIGHT / env->height / 2));
-	printf("Map :\n\n");
+	int	line = 0;//
+
 	y = -1;
+	//printf("Map :\n\n"); //
 	while (++y < env->height)
 	{
 		x = -1;
-		printf("\t");
+	//	printf("\tLine %-2d : ", line); //
 		while (++x < env->width)
 		{
-			printf("%d ", env->grid[y][x].z);
+		//	if (env->grid[y][x].z)
+			//	printf("1 "); //
+		//	else
+			//	printf("0 "); //
 			if (x < env->width - 1)
 				draw_line(transform(env->grid[y][x], env), transform(env->grid[y][x + 1], env), env);
 			if (y < env->height - 1)
 			draw_line(transform(env->grid[y][x], env), transform(env->grid[y + 1][x], env), env);
 		}
-		printf("\n");
+		//printf("\n"); //
+		line++;
 	}
 }
