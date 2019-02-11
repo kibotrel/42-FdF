199d0d2497d46d6671f88464b2f6954558490d1a
Kilian BOTREL <kibotrel@e2r11p16.42.fr>
2019-02-11 21:28:55 +0100

    Removed useless code and start building up menu

--- srcs/print_map.c
+++ srcs/print_map.c
@@ -6,40 +6,39 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/07 05:16:41 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/02/08 00:59:52 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/02/11 21:21:58 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h> //
-
 #include "fdf.h"
 #include "env.h"
+#include "mlx.h"
 
-void	print_map(t_env *env)
+static void	print_controls(void *id, void *win)
+{
+	mlx_string_put(id, win, 1810, 10, 0xFFFFFF, "- Controls -");
+	mlx_string_put(id, win, 1850, 50, 0xFFFFFF, "Move");
+	mlx_string_put(id, win, 1805, 80, 0xFFFFFF, "W / A / S / D");
+	mlx_string_put(id, win, 1840, 110, 0xFFFFFF, "Height");
+	mlx_string_put(id, win, 1795, 140, 0xFFFFFF, "Pg_up / Pg_down");
+}
+
+void		print_map(t_env *env)
 {
 	int	y;
 	int	x;
 
-	int	line = 0;//
-
 	y = -1;
-	//printf("Map :\n\n"); //
+	print_controls(env->mlx->id, env->mlx->win);
 	while (++y < env->height)
 	{
 		x = -1;
-	//	printf("\tLine %-2d : ", line); //
 		while (++x < env->width)
 		{
-		//	if (env->grid[y][x].z)
-			//	printf("1 "); //
-		//	else
-			//	printf("0 "); //
 			if (x < env->width - 1)
 				draw_line(transform(env->grid[y][x], env), transform(env->grid[y][x + 1], env), env);
 			if (y < env->height - 1)
-			draw_line(transform(env->grid[y][x], env), transform(env->grid[y + 1][x], env), env);
+				draw_line(transform(env->grid[y][x], env), transform(env->grid[y + 1][x], env), env);
 		}
-		//printf("\n"); //
-		line++;
 	}
 }
